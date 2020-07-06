88aeb5b4d70bfe1ee362f3b8638644cc1d34aecc
Eklavya Bhatia <eklavyabhatia@gmail.com>
2020-07-07 00:56:14 +0530

    display products from db using api

--- public/script.js
+++ public/script.js
@@ -1,1 +1,28 @@
-console.log('jids')
+function fetchProducts(done) {
+    $.get('/api/products', function (data) {
+        console.log(data)
+        done(data)
+    })
+}
+
+function createProduct(product) {
+    return $(`
+    <div class="card col-3" style="width: 18rem;">
+      <div class="card-body">
+        <h4 class="card-title">${product.name}</h4>
+        <div class="small">${product.manufacturer}</div>
+        <p class="card-text mt-2">${product.price}</p>
+        <button class="btn btn-primary">Buy</button>
+      </div>
+    </div>`)
+}
+
+let productList = $('#product-list')
+
+productList.empty()
+
+fetchProducts(function (products) {
+    for (product of products) {
+        productList.append(createProduct(product))
+    }
+})
