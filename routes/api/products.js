264ac533a8d9cd0f108dd70d71194036cfb0fad3
Eklavya Bhatia <eklavyabhatia@gmail.com>
2020-07-06 23:54:08 +0530

    create products api

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,3 +1,30 @@
+const Products = require('../../db').Products
 const route = require('express').Router()
+
+route.get('/', (req, res) => {
+    Products.findAll()
+        .then((products) => {
+            res.status(200).send(products)
+        })
+        .catch((err) => {
+            res.status(500).send({
+                error: "Error in retreiving products"
+            })
+        })
+})
+
+route.post('/', (req, res) => {
+    Products.create({
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        price: parseFloat(req.body.price)
+    })
+        .then((product) => {
+            res.status(201).send(product)
+        })
+        .catch((err) => {
+            error: "Error in creating product"
+        })
+})
 
 exports = module.exports = route
